a86032d6a7c361bab404057e3dcf325bf4a264bb
Johnny Wei <johntzwei@yahoo.com>
2015-10-27 01:13:53 -0400

    added newspaper scraping script

--- scrape_news.py
+++ scrape_news.py
@@ -1,11 +1,18 @@
+from __future__ import print_function
 import newspaper
 
-fox_news = newspaper.build("http://cnn.com")
+websites = 'http://mitadmissions.org', 
+for url in websites:
+    print("trying to scrape", url)
+    site = newspaper.build(url)
+    print("scraping", url)
 
-for index,article in enumerate(fox_news.articles):
-    article.download()
-    article.parse()
-    with open("article.txt","a") as fh:
-        fh.write(article.text)
+    for index,article in enumerate(site.articles):
+        article.download()
+        article.parse()
+
+        with open("article.txt","a") as fh:
+            fh.write(article.text.encode("latin1", "ignore"))
+        print("article", index, "is now processed")
 
 #end
